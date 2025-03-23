af79786281d5a43b28ee77ae31b5c2561ecef5a3
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-03-23 12:37:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 echo "Welcome to the Ricebowl installer"
 sudo cp -r ./* /usr/bin/
-sudo mv /usr/bin/ricebowl-cli /usr/bin/ricebowl
-sudo chmod +x /usr/bin/ricebowl
+sudo chmod +x /usr/bin/ricebowl-cli
 
 echo "Done! Happy ricing!"
